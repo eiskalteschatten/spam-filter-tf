188d2332f385d5290aaabc123a05688f0ac72820
Alex <alex@alexseifert.com>
2019-02-10 12:56:20 +0100

    Use a dual-way encrypter

--- scripts/encrypt-password.js
+++ scripts/encrypt-password.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const bcrypt = require('bcrypt-nodejs');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('jfio4j39fj4IFJ$($#FJjskjri3ou48oi3nvmk.JFKIJ$(#FJKdsnfio4u8o3');
 
 const password = process.argv[2];
-const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+const encryptedString = cryptr.encrypt(password);
 
-console.log(hash);
+console.log(encryptedString);
