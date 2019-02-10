5eaefcadb4f917f33dfec684c7109b2fda2052f8
Alex <alex@alexseifert.com>
2019-02-10 12:05:31 +0100

    Add some basics to the project

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "promise"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1}
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "off"
+        ],
+        "no-case-declarations": [
+            "off"
+        ],
+        "prefer-const": [
+            "error"
+        ],
+        "arrow-parens": [
+            "error",
+            "as-needed"
+        ],
+        "no-undef": [
+            "off"
+        ],
+        "promise/catch-or-return": "error",
+        "promise/param-names": "error",
+        "promise/no-return-wrap": "error"
+    }
+};
