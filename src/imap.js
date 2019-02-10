e7682595a07ae0b9721614d3c3adbde52fb69aa1
Alex <alex@alexseifert.com>
2019-02-10 14:34:34 +0100

    Use the imap-simple module instead of the imap module

--- src/imap.js
+++ src/imap.js
@@ -1,42 +1,60 @@
 'use strict';
 
-const Imap = require('imap');
+const imaps = require('imap-simple');
 
 const Cryptr = require('cryptr');
 const cryptr = new Cryptr('jfio4j39fj4IFJ$($#FJjskjri3ou48oi3nvmk.JFKIJ$(#FJKdsnfio4u8o3');
 
-const config = require('../account');
+const accountConfig = require('../account');
 
-let imap;
+let imapConnection;
 
-function setupConnection() {
+
+function onmail(numNewMail) {
+    console.log('Checking new email', numNewMail);
+}
+
+function onerror(error) {
+    console.error('There was an error connecting to the IMAP server:\n', error);
+}
+
+function onend() {
+    console.log('The connection to the IMAP server has ended.');
+}
+
+async function setupConnection() {
     console.log('Setting up connection to the IMAP server...');
 
-    const accountConfig = {
-        ...config.account,
-        password: cryptr.decrypt(config.account.password)
-    };
+    try {
+        const config = {
+            ...accountConfig.imapSimple,
+            imap: {
+                ...accountConfig.imapSimple.imap,
+                password: cryptr.decrypt(accountConfig.imapSimple.imap.password)
+            },
+            onmail,
+            onerror,
+            onend
+        };
 
-    imap = new Imap(accountConfig);
+        const { connection, error } = await imaps.connect(config);
+        imapConnection = connection;
 
-    imap.once('ready', () => {
-        console.log('Connected to IMAP server.');
+        if (error) {
+            console.error('There was an error connecting to the IMAP server:', error);
+            return;
+        }
+
+        console.log('Connected to the IMAP server.');
 
 
-    });
-
-    imap.once('error', error => {
-        console.log('There was an error connecting to the IMAP server:', error);
-    });
-
-    imap.once('end', function() {
-        console.log('The connection to the IMAP server has ended.');
-    });
-
-    imap.connect();
+    }
+    catch(error) {
+        console.error('There was an error connecting to the IMAP server:\n', error);
+    }
 }
 
 module.exports = {
     setupConnection,
-    getConnection: () => imap
+    getConnection: () => imapConnection
 };
