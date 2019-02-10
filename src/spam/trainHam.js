a827b24242b2a2953d9dd464938dfe8cf4632b4f
Alex <alex@alexseifert.com>
2019-02-10 15:44:55 +0100

    Start train ham and spam scripts

--- src/spam/trainHam.js
+++ src/spam/trainHam.js
@@ -3,6 +3,33 @@
 const accountConfig = require('../../account');
 const hamFolders = accountConfig.folders.ham;
 
-module.exports = () => {
+module.exports = async () => {
     console.log('Training ham on the following folders:', JSON.stringify(hamFolders));
+
+    try {
+        const ImapConnection = require('../imap');
+        const allMessages = [];
+        const imapConnection = new ImapConnection();
+        const connection = await imapConnection.setupConnection();
+
+        for (const folder of hamFolders) {
+            await connection.openBox(folder);
+            const searchCriteria = ['ALL'];
+
+            const fetchOptions = {
+                bodies: [''],
+                markSeen: false,
+                struct: true
+            };
+
+            const messages = await connection.search(searchCriteria, fetchOptions);
+            allMessages.push(messages);
+        }
+
+
+        connection.end();
+    }
+    catch(error) {
+        throw new Error(error);
+    }
 };
--- src/spam/trainSpam.js
+++ src/spam/trainSpam.js
@@ -3,6 +3,33 @@
 const accountConfig = require('../../account');
 const spamFolders = accountConfig.folders.spam;
 
-module.exports = () => {
+
+module.exports = async () => {
     console.log('Training spam on the following folders:', JSON.stringify(spamFolders));
+
+    try {
+        const ImapConnection = require('../imap');
+        const allMessages = [];
+        const imapConnection = new ImapConnection();
+        const connection = await imapConnection.setupConnection();
+
+        for (const folder of spamFolders) {
+            await connection.openBox(folder);
+            const searchCriteria = ['ALL'];
+
+            const fetchOptions = {
+                bodies: [''],
+                markSeen: false,
+                struct: true
+            };
+
+            const messages = await connection.search(searchCriteria, fetchOptions);
+            allMessages.push(messages);
+        }
+
+        connection.end();
+    }
+    catch(error) {
+        throw new Error(error);
+    }
 };
